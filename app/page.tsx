8b1e114f1a4130b79bfd8a6ee24559e3e01f5d24
Eric Rothmuller <eric@skymarketingco.com>
2025-08-13 07:15:08 -0700

    Edited text on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            The Future of Funeral Funding.
+            The Future of Funeral Funding. VIP Funeral Funding.
           </li>
         </ol>
 
