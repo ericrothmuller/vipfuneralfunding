dbdd5a6dd09d11694a5d884b5612687eadc2a292
Eric Rothmuller <eric@skymarketingco.com>
2025-08-14 16:14:41 -0700

    Added logo and verbiage to the login screen

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,25 @@ import { redirect } from "next/navigation";
 import { getUserFromCookie } from "@/lib/auth";
 import LoginForm from "@/components/LoginForm";
 
-export default function Home() {
+export default function Home() {  
   const user = getUserFromCookie();
   if (user) redirect("/dashboard"); // already logged in → skip login
-  return <LoginForm />;
+  return (  
+        <div className="centered">
+            <main>
+              <br/>
+              <Image
+                src="/VIP-Funeral-Funding-Logo-Gold.png"
+                alt="VIP Funeral Funding logo"
+                width={898}
+                height={152}
+                style={{ width: "30%", height: "auto" }}
+                priority
+              />
+              <LoginForm />
+            </main>
+            <footer>
+              The Future of Funeral Funding. Coming Soon.
+            </footer>
+        </div>);
 }
